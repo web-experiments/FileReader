0c74c02c4c9371d86e46433a86c5b4fc1211b36c
Andre <Andre@ANDRE-PC1>
2020-08-05 13:59:36 +0200

    Scrollbar hinzugefügt

--- FileReader/MainWindow.xaml.cs
+++ FileReader/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace FileReader
 {
@@ -23,6 +25,18 @@ namespace FileReader
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+ 
+
+        private void BtnOpenFileDialog_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if(openFileDialog.ShowDialog() == true)
+            {
+                Analyse.Text = File.ReadAllText(openFileDialog.FileName);
+
+            }
         }
     }
 }
