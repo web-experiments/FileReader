398ee49817563dedcb199e6826b5d9ce73b6b5ba
Andre <Andre@ANDRE-PC1>
2020-08-20 09:29:25 +0200

    Livechart hinzugefügt

--- FileReader/MainWindow.xaml.cs
+++ FileReader/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using LiveCharts;
+using LiveCharts.Wpf;
 using Microsoft.Win32;
 
 namespace FileReader
@@ -22,10 +24,41 @@ namespace FileReader
     /// </summary>
     public partial class MainWindow : Window
     {
+        public string[] Labels { get; set; }
+        public SeriesCollection SeriesCollection { get; set; }
         public MainWindow()
         {
             InitializeComponent();
             setDefaultValues();
+
+          
+            SeriesCollection = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Title = "Series 1",
+                    Values = new ChartValues<double> { 4, 6, 5, 2 ,4 }
+                },
+                new LineSeries
+                {
+                    Title = "Series 2",
+                    Values = new ChartValues<double> { 6, 7, 3, 4 ,6 },
+                    PointGeometry = null
+                },
+                new LineSeries
+                {
+                    Title = "Series 3",
+                    Values = new ChartValues<double> { 4,2,7,2,7 },
+                    PointGeometry = null
+                }
+            };
+
+            
+
+
+     
+
+            DataContext = this;
         }
 
         private void setDefaultValues()
@@ -42,8 +75,9 @@ namespace FileReader
                 Analyse.Text = File.ReadAllText(openFileDialog.FileName);
                 int counter = 0;
                 string line;
+                int numberofcalls = 0;
 
-                System.IO.StreamReader file = new System.IO.StreamReader(openFileDialog.FileName);
+        System.IO.StreamReader file = new System.IO.StreamReader(openFileDialog.FileName);
                 while ((line = file.ReadLine()) != null)
                 {
                     if (counter > 9)
@@ -56,9 +90,15 @@ namespace FileReader
                         string[] data = line.Split(new char[0], StringSplitOptions.RemoveEmptyEntries);
                         var parsedDate = DateTime.Parse(data[0]);
                         System.Console.WriteLine(parsedDate.DayOfWeek);
+                        if(Int16.Parse(data[2]) >= 1)
+                        {
+                            numberofcalls = numberofcalls + Int16.Parse(data[2]);
+                        }
                     }
                     counter++;
                 }
+                Labels = new[] { "Jan", "Feb", "Mar", "Apr", "May" };
+                System.Console.WriteLine(numberofcalls);
 
             }
         }
